d975a856c71e9672ae4dace8b2779b5a3fa5165f
Nisarg Dewang <nisarg.dewang@biztechcs.com>
2018-11-30 17:05:09 +0530

    developing the TimeConversion feature and adding unit tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
-function sayHiTo(name) {
-    return `Hi, ${name}`;
-  }
-  
-  const message = sayHiTo('Bruno');
-  
-  console.log(message);
+const errMsg = 'TimeString is not valid. please enter TimeString in (hh:mm:ss) format.'
+const numericErr = 'Please enter numeric Value.'
+
+module.exports = {
+    ConvertMstoTimeString: (ms) => {
+        if (isNaN(ms)) {
+            return numericErr
+        } else {
+            let seconds = ms / 1000;
+            let hours = parseInt(seconds / 3600);
+            seconds = seconds % 3600;
+            let minutes = parseInt(seconds / 60);
+            seconds = Math.round(seconds % 60)
+            if (hours < 10) hours = '0' + hours
+            if (minutes < 10) minutes = '0' + minutes
+            if (seconds < 10) seconds = '0' + seconds
+            return (hours + ":" + minutes + ":" + seconds);
+        }
+    }, ConvertTimeStringtoMs: (timestring) => {
+        if (timestring.indexOf(':') > -1) {
+            let a = timestring.split(':')
+            let mSeconds
+            if (a.length === 1) {
+                mSeconds = (+a[2]);
+            }
+            else if (a.length === 1) {
+                mSeconds = (+a[1]) * 60 + (+a[2]);
+            } else {
+                mSeconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+            }
+            return mSeconds * 1000
+        } else {
+            return errMsg
+        }
+    }
+}
--- test/index.js
+++ test/index.js
@@ -1,9 +1,38 @@
 import assert from 'assert';
 
+const { ConvertMstoTimeString, ConvertTimeStringtoMs } = require('../src/index');
+
+const numErr = 'Please enter numeric Value.';
+const stringErr = 'TimeString is not valid. please enter TimeString in (hh:mm:ss) format.';
+
+const testSamples1 = [
+  { input: 'abc', expectedResult: numErr, description: 'should return error message when receiving "abc"' },
+  { input: 'abc1234567', expectedResult: numErr, description: 'should return error message when receiving "abc1234567"' },
+  { input: '127438@#', expectedResult: numErr, description: 'should return error message when receiving "127438@#"' },
+  { input: '1234567', expectedResult: '00:20:35', description: 'should return 00:20:35' },
+  { input: '23456733', expectedResult: '06:30:57', description: 'should return 06:30:57' },
+];
+
+const testSamples2 = [
+  { input: 'abc', expectedResult: stringErr, description: 'should return error message when receiving "abc"' },
+  { input: 'abc1234567', expectedResult: stringErr, description: 'should return error message when receiving "abc1234567"' },
+  { input: '127438@#', expectedResult: stringErr, description: 'should return error message when receiving "127438@#"' },
+  { input: '12:01:30', expectedResult: '43290000', description: 'should return 00:20:35' },
+  { input: '06:30:24', expectedResult: '23424000', description: 'should return 06:30:57' },
+];
+
 describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+  testSamples1.forEach((sample) => {
+    it(sample.description, () => {
+      assert.equal(ConvertMstoTimeString(sample.input), sample.expectedResult);
+    });
+  });
+});
+
+describe('Array', () => {
+  testSamples2.forEach((sample) => {
+    it(sample.description, () => {
+      assert.equal(ConvertTimeStringtoMs(sample.input), sample.expectedResult);
     });
   });
 });
