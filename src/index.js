6c120ae81e1f0d94c6604352860f6dc24f246295
Nisarg Dewang <nisarg.dewang@biztechcs.com>
2018-12-01 18:44:09 +0530

    Changed the config files

--- src/index.js
+++ src/index.js
@@ -1,37 +1,38 @@
-const errMsg = 'TimeString is not valid. please enter TimeString in (hh:mm:ss) format.'
-const numericErr = 'Please enter numeric Value.'
+const errMsg = 'TimeString is not valid. please enter TimeString in (hh:mm:ss) format.';
+const numericErr = 'Please enter numeric Value.';
 
-module.exports = {
-    ConvertMstoTimeString: (ms) => {
-        if (isNaN(ms)) {
-            return numericErr
-        } else {
-            let seconds = ms / 1000;
-            let hours = parseInt(seconds / 3600);
-            seconds = seconds % 3600;
-            let minutes = parseInt(seconds / 60);
-            seconds = Math.round(seconds % 60)
-            if (hours < 10) hours = '0' + hours
-            if (minutes < 10) minutes = '0' + minutes
-            if (seconds < 10) seconds = '0' + seconds
-            return (hours + ":" + minutes + ":" + seconds);
-        }
-    }, ConvertTimeStringtoMs: (timestring) => {
-        let ts = /^[0-9/:]*$/
-        if (ts.test(timestring) === true) {
-            let a = timestring.split(':')
-            let mSeconds
-            if (a.length === 1) {
-                mSeconds = (+a[0]);
-            }
-            else if (a.length === 2) {
-                mSeconds = (+a[0]) * 60 + (+a[1]);
-            } else {
-                mSeconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
-            }
-            return mSeconds * 1000
-        } else {
-            return errMsg
-        }
-    }
+function ConvertMstoTimeString(ms) {
+  const ts = /^[0-9/]*$/;
+  if (ms.test(ts) === false) {
+    return numericErr;
+  }
+  let seconds = ms / 1000;
+  let hours = parseInt(seconds / 3600, 0);
+  seconds %= 3600;
+  let minutes = parseInt(seconds / 60, 0);
+  seconds = Math.round(seconds % 60);
+  if (hours < 10) hours = `0${hours}`;
+  if (minutes < 10) minutes = `0${minutes}`;
+  if (seconds < 10) seconds = `0${seconds}`;
+  return (`${hours}:${minutes}:${seconds}`);
 }
+
+function ConvertTimeStringtoMs(timestring) {
+  const ts = /^[0-9]*$/;
+  if (ts.test(timestring) === true) {
+    const a = timestring.split(':');
+    let mSeconds;
+    if (a.length === 1) {
+      mSeconds = (+a[0]);
+    } else if (a.length === 2) {
+      mSeconds = (+a[0]) * 60 + (+a[1]);
+    } else {
+      mSeconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+    }
+    return mSeconds * 1000;
+  }
+  return errMsg;
+}
+
+exports.ConvertMstoTimeString = ConvertMstoTimeString;
+exports.ConvertTimeStringtoMs = ConvertTimeStringtoMs;
